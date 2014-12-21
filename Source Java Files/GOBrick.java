b48a6a6f399ad054234d9d99965422369ee3968d
Nathaniel Singewald <thanoyk97@gmail.com>
2014-12-21 11:15:05 -0600

    Breakout 1.1
    Small changes to brick handling

--- Source Java Files/GOBrick.java
+++ Source Java Files/GOBrick.java
@@ -1,7 +1,6 @@
 package breakout;
 
 public class GOBrick extends GameObject{
-	private boolean isBroken = false;
 	private GOBall ball;
 	private int BrickIndex;
 	private Game game;
@@ -18,20 +17,15 @@ public class GOBrick extends GameObject{
 
 	@Override
 	void update() {
-		if(!isBroken){
-			if(Physics.checkCollison(this, ball)){
-				ball.reverseY(this.getCenterX());
-				smash();
-			}
+		if(Physics.checkCollison(this, ball)){
+			ball.reverseY(this.getCenterX());
+			smash();
 		}
 		
 	}
 
 	private void smash(){
-		isBroken = true;
 		game.removeBrick(BrickIndex);
-		this.x = -500;
-		this.y = -500;
 		game.IncreaseScore();
 	}
 }
--- Source Java Files/GONothing.java
+++ Source Java Files/GONothing.java
@@ -2,7 +2,6 @@ package breakout;
 
 public class GONothing extends GameObject{
 	public GONothing(){
-		this.color = new Color(1f,1f,1f);
 	}
 	@Override
 	void update() {
